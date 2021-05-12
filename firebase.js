b087eecac8bcb42d2d4d9a8fc6d4572ede027cf8
LailsonMafra <68885720+LailsonMafra@users.noreply.github.com>
2021-05-12 13:38:23 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCvEv7AropwWlIEC-w5c3JgprOPxjA8-kM",
+  authDomain: "todolist-84473.firebaseapp.com",
+  databaseURL: "https://todolist-84473.firebaseio.com",
+  projectId: "todolist-84473",
+  storageBucket: "todolist-84473.appspot.com",
+  messagingSenderId: "1009074245486",
+  appId: "1:1009074245486:web:f573ce9d33c653b28ebb7f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
